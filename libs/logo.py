788b4ff3e60c2c06bcce63e840f9c28dbf696804
Guccifer Shubham <65662155+shubhushubhu99@users.noreply.github.com>
2022-02-08 20:28:49 +0530

    Add files via upload

---
+++ libs/logo.py
@@ -0,0 +1,35 @@
+# coding=utf-8
+#!/usr/bin/env python3
+from random import choice
+from colorama import  Fore,Style,Back
+
+logo =  """ 
+
+██████╗  █████╗ ██████╗ ██╗  ██╗
+██╔══██╗██╔══██╗██╔══██╗██║ ██╔╝
+██║  ██║███████║██████╔╝█████╔╝ 
+██║  ██║██╔══██║██╔══██╗██╔═██╗ 
+██████╔╝██║  ██║██║  ██║██║  ██╗
+╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝
+                                
+███████╗ ██████╗ ██╗   ██╗ █████╗ ██████╗ 
+██╔════╝██╔═══██╗██║   ██║██╔══██╗██╔══██╗
+███████╗██║   ██║██║   ██║███████║██║  ██║
+╚════██║██║▄▄ ██║██║   ██║██╔══██║██║  ██║
+███████║╚██████╔╝╚██████╔╝██║  ██║██████╔╝
+╚══════╝ ╚══▀▀═╝  ╚═════╝ ╚═╝  ╚═╝╚═════╝ 
+
+█ █▄░█ █▀ ▀█▀ ▄▀█
+█ █░▀█ ▄█ ░█░ █▀█
+
+█▀█ █▀▀ █▀█ █▀█ █▀█ ▀█▀ █▀▀ █▀█
+█▀▄ ██▄ █▀▀ █▄█ █▀▄ ░█░ ██▄ █▀▄
+                                  
+  """
+def print_logo():
+    print(Fore.RED + Style.BRIGHT + logo + Style.RESET_ALL + Style.BRIGHT +"\n")
+    print(Style.RESET_ALL + Style.BRIGHT, Style.BRIGHT)                                                  
+    print(Fore.RED + "                                                      Youtube" + Fore.CYAN + " - Shubhu Cyber Studio ")
+    print(Fore.RED + "                                                      BY     " + Fore.CYAN + " - @Guccifer Shubham")
+    print(Fore.RED + "                                                      Telegram"+ Fore.CYAN + " - t.me/darksquadbyshub1hacker ")
+    print(Fore.GREEN + "      Dark Squad Project: https://t.me/darksquadbyshub1hacker"+ Style.RESET_ALL + Style.BRIGHT)
